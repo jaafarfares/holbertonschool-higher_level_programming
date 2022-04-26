a9e1a02f475534a18d0753330759f439d9bcbc14
jaafarfares <jaafarferes@gmail.com>
2022-04-26 15:46:18 +0100

    assign a random signed number to the variable and print the last digit of the number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-digit = abs(number) % 10
+last_digit = abs(number) % 10
 if number < 0:
-    digit = -digit
-    print("Last digit of {} is {} and is ".format(number, digit), end="")
-if digit > 5:
-    print("greater than 5")
-elif digit == 0:
-    print("0")
+    last_digit = -last_digit
+    if last_digit > 5:
+        print("Last digit of", str(number), "is", last_digit,  "\
+                and is greater than 5")
+if last_digit == 0:
+    print("Last digit of", str(number), "is", last_digit, "and is 0")
 else:
-    print("less than 6 and not 0")
+    print("Last digit of", str(number), "is", last_digit, "\
+and is less than 6 and not 0")
