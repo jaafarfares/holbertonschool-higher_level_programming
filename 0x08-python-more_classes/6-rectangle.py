b0918b24dac3f9b9d636e440dc2cc7269d5c3646
jaafarfares <jaafarferes@gmail.com>
2022-05-23 22:14:46 +0100

    Write a class Rectangle that defines a rectangle by: (based on 6-rectangle.py)

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -13,30 +13,30 @@ class Rectangle():
     @property
     def width(self):
         """width property """
-        return(self.__width)
+        return self.__width
 
     @width.setter
     def width(self, value):
         """ width setter"""
         if type(value) is not int:
-            raise(TypeError("width must be an integer"))
+            raise TypeError("width must be an integer")
         elif value < 0:
-            raise(ValueError("width must be >= 0"))
+            raise ValueError("width must be >= 0")
         else:
             self.__width = value
 
     @property
     def height(self):
         """height property"""
-        return(self.__height)
+        return self.__height
 
     @height.setter
     def height(self, value):
         """height setter """
         if type(value) is not int:
-            raise(TypeError("height must be an integer"))
+            raise TypeError("height must be an integer")
         elif value < 0:
-            raise(ValueError("height must be >= 0"))
+            raise ValueError("height must be >= 0")
         else:
             self.__height = value
 
@@ -47,12 +47,14 @@ class Rectangle():
     def perimeter(self):
         """return the rectangle perimiter"""
         if self.width == 0 or self.height == 0:
-            return 0
+            return ""
         else:
             return 2*(self.__height + self.__width)
 
     def __str__(self):
         """ print the rectangle area with the charchter '#' """
+        if self.width == 0 or self.height == 0:
+            return ""
         return '\n'.join('#' * self.width for _ in range(self.height))
 
     def __repr__(self):
