d0fffcf6bcbbfa2f31ff1b51fc3b3770483fee23
jaafarfares <jaafarferes@gmail.com>
2022-05-23 22:11:10 +0100

    test

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -50,5 +50,5 @@ class Rectangle():
     def __str__(self):
         """ print the rectangle area with the charchter '#' """
         if self.width == 0 or self.height == 0:
-            return 0
+            return ""
         return '\n'.join('#' * self.width for _ in range(self.height))
