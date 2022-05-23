f914e8b702fde9ba04bd296b11419d6cc2965056
jaafar fares <98318912+jaafarfares@users.noreply.github.com>
2022-05-23 19:21:06 +0100

    Update 3-rectangle.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -18,10 +18,9 @@ class Rectangle():
         """ width setter"""
         if type(value) is not int:
             raise(TypeError("width must be an integer"))
-        elif value < 0:
+        if value < 0:
             raise(ValueError("width must be >= 0"))
-        else:
-            self.__width = value
+        self.__width = value
 
     @property
     def height(self):
@@ -33,10 +32,9 @@ class Rectangle():
         """height setter """
         if type(value) is not int:
             raise(TypeError("height must be an integer"))
-        elif value < 0:
+        if value < 0:
             raise(ValueError("height must be >= 0"))
-        else:
-            self.__height = value
+        self.__height = value
 
     def area(self):
         """ return the rectangle area"""
