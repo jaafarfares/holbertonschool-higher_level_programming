ab79488f8250b2ccc113c68d086458b3309322c5
jaafarfares <jaafarferes@gmail.com>
2022-05-31 11:26:30 +0100

    Write a function that writes a string

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Write a function that writes a string
+to a text file (UTF8) and returns the
+number of characters written:"""
+
+
+def write_file(filename="", text=""):
+    """the function parameter to open the file and write the string"""
+    with open('my_first_file.txt', 'w', encoding='utf-8') as f:
+        s = str(text)
+        f.write(s)
+        return len(text)
