6dbc1b1e75802460ee99d5d22dbbb2c844daa0a8
jaafarfares <jaafarferes@gmail.com>
2022-06-06 14:21:27 +0100

    test

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,6 +4,7 @@ from . rectangle import Rectangle
 
 class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
+        self.size = size
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
