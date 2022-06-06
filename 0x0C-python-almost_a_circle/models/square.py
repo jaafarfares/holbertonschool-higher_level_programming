adc79244cadc01c3cea9f89f2b77290612626450
jaafarfares <jaafarferes@gmail.com>
2022-06-06 14:27:46 +0100

    test

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ Square module """
+from . base import Base
 from . rectangle import Rectangle
+
 
 class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
@@ -31,25 +33,26 @@ class Square(Rectangle):
             self.__height = value
 
     def update(self, *args, **kwargs):
-        if len(args) > 0:
-            for i in range(len(args)):
-                if i == 0:
-                    self.id = args[0]
-                if i == 1:
-                    self.size = args[1]
-                if i == 2:
-                    self.x = args[2]
-                if i == 3:
-                    self.y = args[3]
+        if args is not None and len(args) != 0:
+            if len(args) >= 1:
+                if type(args[0]) != int and args[0] is not None:
+                    raise TypeError("id must be an integer")
+                self.id = args[0]
+            if len(args) > 1:
+                self.size = args[1]
+            if len(args) > 2:
+                self.x = args[2]
+            if len(args) > 3:
+                self.y = args[3]
         else:
-            if len(kwargs) > 0:
-                keys = kwargs.items()
-                for i in keys:
-                    if i == "id":
-                        self.id = keys
-                    if i == "size":
-                        self.size = keys
-                    if i == "x":
-                        self.x = keys
-                    if i == "y":
-                        self.y = keys
+            for key, value in kwargs.items():
+                if key == "id":
+                    if type(value) != int and value is not None:
+                        raise TypeError("id must be an integer")
+                    self.id = value
+                if key == "size":
+                    self.size = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = 
