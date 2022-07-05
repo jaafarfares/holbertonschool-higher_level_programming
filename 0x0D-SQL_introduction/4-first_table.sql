3e17814c24179ec1df0dcb652e33ef589cd0788f
root <root@JaafarLaptop.localdomain>
2022-07-05 19:37:40 +0100

    create first table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,6 @@
 -- script that creates a table in the current database
-CREATE TABLE IF NOT EXISTS first_table first_table (id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table 
+(
+    id INT,
+    name VARCHAR(256)
+);
