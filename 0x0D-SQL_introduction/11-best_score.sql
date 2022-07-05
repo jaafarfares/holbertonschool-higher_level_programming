b03ac91fff64f6fa63c3427c8c1f728785a2d4b6
root <root@JaafarLaptop.localdomain>
2022-07-05 21:16:18 +0100

    push first check later

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,2 @@
 -- script that lists all records with a score >= 10
-SELECT score FROM second_table
-WHERE score >= 10 ORDER BY score DESC;
+SELECT score FROM second_table WHERE score >= 10 ORDER BY DESC;
