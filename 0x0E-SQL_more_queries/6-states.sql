8a0d3109c0ee09a23f795f6f19e170a2d389ddcc
root <root@JaafarLaptop.localdomain>
2022-07-06 15:08:14 +0100

    unique id not null and primmary key

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- create a dataabase and a table
+CREATE dataabase IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE if NOT EXISTS states
+(
+    id INT UNIQUE NOT NULL PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+)
