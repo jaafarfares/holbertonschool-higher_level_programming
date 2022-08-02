18dca1f47b10760dab294643b0a58a568944487b
jaaferfares <jaafarferes@gmail.com>
2022-08-02 14:09:27 +0100

    Write a script that prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,11 +2,12 @@
 const { argv } = require('process');
 if (isNaN(argv[2])) {
   console.log('Missing size');
-}
-let s = '';
-for (let i = 0; i < argv[2]; i++) {
-  s += 'x';
-}
-for (let a = 0; a < argv[2]; a++) {
-  console.log(s);
+} else {
+  let s = '';
+  for (let i = 0; i < argv[2]; i++) {
+    s += 'x';
+  }
+  for (let a = 0; a < argv[2]; a++) {
+    console.log(s);
+  }
 }
