e250903fb2c28de669cfdf509382919021f4fb3b
jaaferfares <jaafarferes@gmail.com>
2022-08-03 12:05:19 +0100

    Write a class Square that defines a square and inherits from Rectangle of 4-rectangle.js:

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangl.js');
+const Rectangle = require('./4-rectangle.js');
 
 module.exports = class Square extends Rectangle {
   constructor (size) {
