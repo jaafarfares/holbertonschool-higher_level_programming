d4f4e10c9421a30282ffe6de91dd70cc3f40e04a
jaaferfares <jaafarferes@gmail.com>
2022-08-04 14:18:45 +0100

    Write a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Write a script that lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    """ now lets connect to the database"""
+    db = MySQLdb.connect(
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+    )
+    """ i didnt assgin the *host value and the *port value in the arguments
+    cuz they will be ther bye default """
+cursor = db.cursor()
+cursor.execute("SELECT * FROM states".format(argv[4]))
+result = cursor.fetchall()
+for i in result:
+    if i[1] == argv[4]:
+        """ lets loop through the states and print the once that starts with *N """
+        print(i)
+cursor.close()
+""" we have to close the cursor"""
+db.close()
+""" and finally close the database"""
