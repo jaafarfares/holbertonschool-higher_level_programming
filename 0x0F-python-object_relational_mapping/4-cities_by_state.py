3da2227d465c1db1c94af66f2c57ecfeadd96725
jaaferfares <jaafarferes@gmail.com>
2022-08-07 08:14:46 +0100

    Write a script that lists all cities from the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -12,16 +12,17 @@ if __name__ == "__main__":
     )
     """ i didnt declare the *host and the *port arguments
     cuz they will be ther bye default """
-cursor = db.cursor()
-cursor.execute(
-    "SELECT cities.id, cities.name, states.name FROM cities" +
-    " INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id ")
-""" we only select cities id first and then cities name and states names"""
-result = cursor.fetchall()
-for i in result:
-    """ lets loop through the states"""
-    print(i)
-cursor.close()
-""" we have to close the cursor"""
-db.close()
-""" and finally close the database"""
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT cities.id, cities.name, states.name FROM cities" +
+        " INNER JOIN states ON cities.state_id = states.id ORDER BY" +
+        "cities.id ")
+    """ we only select cities id first and then cities name and states names"""
+    result = cursor.fetchall()
+    for i in result:
+        """ lets loop through the states"""
+        print(i)
+    cursor.close()
+    """ we have to close the cursor"""
+    db.close()
+    """ and finally close the database"""
