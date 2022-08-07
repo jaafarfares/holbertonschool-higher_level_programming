259c842a44968f78a2ccde3123a298cff6c6bb76
jaaferfares <jaafarferes@gmail.com>
2022-08-07 19:59:31 +0100

    test

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -7,17 +7,22 @@ the comment below is for the default implementation
 from sqlalchemy.orm import Session
 from sys import argv
 from model_state import Base, State
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine, update
+
 if __name__ == "__main__":
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]))
     Base.metadata.create_all(engine)
 
     session = Session(engine)
-    neww = State(name="Louisiana")
-    session.add(neww)
+    stmt = (
+    update(State).
+    where(id == 2).
+    values(name='New Mexico')
+)
+    session.add(stmt)
     session.commit()
-    print(neww.id)
+    print(stmt.id)
     session.close()
 
     """ finaly lets close the session """
