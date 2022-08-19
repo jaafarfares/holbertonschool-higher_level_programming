0dcc3dae296998f3e1cb320ae1bdfab966aedfbd
jaafarfares <jaafarferes@gmail.com>
2022-08-19 11:03:49 +0100

    sends a POST request to the passed URL with the email as a parameter

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 """
-displays the email return by a post request
+sends a POST request to the passed URL with the email as a parameter
 """
-import urllib.parse as p
-import urllib.request as r
+import urllib.parse
+import urllib
+import urllib.request
 from sys import argv
 
 
-if __name__ == '__main__':
-    values = {
+ur = argv[1]
+maiil = {
         'email': argv[2]
     }
-    data = p.urlencode(values)
-    data = data.encode('ascii')
-    req = r.Request(argv[1], data)
-    with r.urlopen(req) as res:
-        body = res.read().decode('utf-8')
-        print(body)
+data = urllib.parse.urlencode(maiil)
+data = data.encode('ascii')
+al = urllib.request.Request(ur, data)
+with urllib.request.urlopen(al) as f:
+    print(i.read().decode('utf-8'))
