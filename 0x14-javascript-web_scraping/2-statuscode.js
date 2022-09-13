9280ff9d09b9ef64ed1f428949cccd24027b8618
jaafarfares <jaafarferes@gmail.com>
2022-09-13 13:13:42 +0100

    get the request response code using axios

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const argv = process.argv;
 const request = require('request');
-request(argv[2], function (error, response) {
+request(argv[2], function (error, response, body) {
   console.log('code: ' + response.statusCode);
 });
